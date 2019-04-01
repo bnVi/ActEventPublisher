35de1ca43773ba23e1d0a318802527030e1b2ec2
bnVi <bnvi.54+github@gmail.com>
2019-03-31 20:33:31 -0600

    named the groupbox

--- ActEventPublisher/UserControls/SettingsUserControl.Designer.cs
+++ ActEventPublisher/UserControls/SettingsUserControl.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.updateEndpointButton = new System.Windows.Forms.Button();
             this.endpointTextBox = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox1.SuspendLayout();
+            this.endpointGroupBox = new System.Windows.Forms.GroupBox();
+            this.endpointGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // updateEndpointButton
@@ -51,26 +51,26 @@
             this.endpointTextBox.Size = new System.Drawing.Size(297, 20);
             this.endpointTextBox.TabIndex = 2;
             // 
-            // groupBox1
+            // endpointGroupBox
             // 
-            this.groupBox1.Controls.Add(this.endpointTextBox);
-            this.groupBox1.Controls.Add(this.updateEndpointButton);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(371, 52);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Endpoint";
+            this.endpointGroupBox.Controls.Add(this.endpointTextBox);
+            this.endpointGroupBox.Controls.Add(this.updateEndpointButton);
+            this.endpointGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.endpointGroupBox.Name = "endpointGroupBox";
+            this.endpointGroupBox.Size = new System.Drawing.Size(371, 52);
+            this.endpointGroupBox.TabIndex = 5;
+            this.endpointGroupBox.TabStop = false;
+            this.endpointGroupBox.Text = "Endpoint";
             // 
             // SettingsUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.endpointGroupBox);
             this.Name = "SettingsUserControl";
             this.Size = new System.Drawing.Size(636, 324);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.endpointGroupBox.ResumeLayout(false);
+            this.endpointGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -79,6 +79,6 @@
 
         private System.Windows.Forms.Button updateEndpointButton;
         private System.Windows.Forms.TextBox endpointTextBox;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox endpointGroupBox;
     }
 }
